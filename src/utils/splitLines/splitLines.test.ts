85011fcbd91687b3659f78733118acfec7a45e6b
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2022-03-22 08:36:19 -0400

    Add tagged template literals test

--- src/utils/splitLines/splitLines.test.ts
+++ src/utils/splitLines/splitLines.test.ts
@@ -13,4 +13,20 @@ describe("splitLines", () => {
     expect(lines[1]).toEqual("second line");
     expect(lines[2]).toEqual("third line");
   });
+
+  it("splits a tagged template literal into an array of lines", () => {
+    // ARRANGE
+    const INPUT = `first line
+    second line
+    third line
+    `;
+
+    // ACT
+    const lines = splitLines(INPUT);
+
+    // ASSERT
+    expect(lines[0]).toEqual("first line");
+    expect(lines[1].trim()).toEqual("second line");
+    expect(lines[2].trim()).toEqual("third line");
+  });
 });
